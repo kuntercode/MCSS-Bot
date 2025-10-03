d12919b7919c9c3a105286a9f5b05fb914efee25
Kunter <pinhastern@gmail.com>
2025-10-03 17:34:28 +0300

    added real time status

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 from mcstatus import JavaServer
 import discord
-from discord.ext import commands
+from discord.ext import tasks, commands
 import yaml
 import subprocess
 import asyncio
@@ -10,10 +10,7 @@ import asyncio
 with open("config.yaml", "r") as file:
     config = yaml.safe_load(file)
 
-
 bot = discord.Bot()
-
-server_process = None
 
 server = JavaServer.lookup(config["server_address"])
 
@@ -27,11 +24,11 @@ embed = discord.Embed(
 embed.set_author(name="Furry Team", icon_url="https://cdn.discordapp.com/attachments/1033783709364670464/1419624423610122352/avatar_e3d00202-c001-4ab3-9f50-32baf29d0fd8.png?ex=68d26fc5&is=68d11e45&hm=12e224421976bd93fe94e706a966c8e88d2c77e3c47cfa449101ef960f6f8908&")
 
 
-
 @bot.event
 async def on_ready():
     print(f"bot ready! logged in as {bot.user}")
-    await bot.change_presence(status=discord.Status.idle, activity=discord.Activity(type=discord.ActivityType.listening, name="/startserver"))
+    #await bot.change_presence(status=discord.Status.idle, activity=discord.Activity(type=discord.ActivityType.listening, name="/startserver"))
+    check_server_status.start()
 
 
 @bot.event
@@ -40,6 +37,20 @@ async def on_application_command_error(ctx, error):
         await ctx.respond(f"This command is on cooldown! Try again in {error.retry_after:.2f} seconds.", ephemeral=True)
     else:
         raise error
+
+
+@tasks.loop(seconds=7)
+async def check_server_status():
+    global server
+
+    try:
+        status = await asyncio.wait_for(server.async_status(), timeout=10.0)
+        print("server online")
+        await bot.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.watching, name=f"Server, {status.players.online}/{status.players.max} online"))
+
+    except Exception:
+        print("server offline")
+        await bot.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.listening, name="/startserver"))
 
 
 
@@ -56,7 +67,7 @@ async def serverstatus(ctx: discord.ApplicationContext):
         embed.description = f"The server is online, `{status.players.online}/{status.players.max}` players playing."
         await ctx.followup.send(embed=embed, ephemeral=True)
 
-    except Exception as e:
+    except Exception:
         embed.title = "Server Status"
         embed.description = f"The server is offline."
         await ctx.followup.send(embed=embed, ephemeral=True)
