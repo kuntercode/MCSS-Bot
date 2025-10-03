0920f7619a087907b45f5621c27c91ed5dd7c3c6
Kunter <pinhastern@gmail.com>
2025-10-03 18:13:02 +0300

    made some responses public

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ server = JavaServer.lookup(config["server_address"])
 embed = discord.Embed(
     title="[title]",
     description="[description]",
-    color=discord.Colour.blurple(),
+    color=discord.Colour.green(),
 )
 
 embed.set_author(name="Furry Team", icon_url="https://cdn.discordapp.com/attachments/1033783709364670464/1419624423610122352/avatar_e3d00202-c001-4ab3-9f50-32baf29d0fd8.png?ex=68d26fc5&is=68d11e45&hm=12e224421976bd93fe94e706a966c8e88d2c77e3c47cfa449101ef960f6f8908&")
@@ -39,7 +39,7 @@ async def on_application_command_error(ctx, error):
         raise error
 
 
-@tasks.loop(seconds=7)
+@tasks.loop(seconds=45)
 async def check_server_status():
     global server
 
@@ -57,18 +57,18 @@ async def serverstatus(ctx: discord.ApplicationContext):
     global embed
     global server
 
-    await ctx.defer(ephemeral=True)
+    await ctx.defer()
     
     try:
         status = await asyncio.wait_for(server.async_status(), timeout=10.0)
         embed.title = "Server Status"
         embed.description = f"The server is online, `{status.players.online}/{status.players.max}` players playing."
-        await ctx.followup.send(embed=embed, ephemeral=True)
+        await ctx.followup.send(embed=embed)
 
     except Exception:
         embed.title = "Server Status"
         embed.description = f"The server is offline."
-        await ctx.followup.send(embed=embed, ephemeral=True)
+        await ctx.followup.send(embed=embed)
 
 
 
@@ -78,20 +78,19 @@ async def startserver(ctx: discord.ApplicationContext):
     global embed
     global server
 
-    await ctx.defer(ephemeral=True)
+    await ctx.defer()
     
     try:
         status = await asyncio.wait_for(server.async_status(), timeout=10.0)
         embed.title = "Server is already running"
         embed.description = f"The server has `{status.players.online}/{status.players.max}` players online."
-        await ctx.followup.send(embed=embed, ephemeral=True)
+        await ctx.followup.send(embed=embed)
 
     except Exception as e:
         subprocess.Popen(config["start_command"], creationflags=subprocess.CREATE_NEW_CONSOLE)
         embed.title = "Server offline, starting..."
         embed.description = "This might take a few moments, please be patient."
-        await bot.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.watching, name="Server Online"))
-        await ctx.followup.send(embed=embed, ephemeral=True)
+        await ctx.followup.send(embed=embed)
 
 
 # login to discord
